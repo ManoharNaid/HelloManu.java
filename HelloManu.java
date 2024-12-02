b6d71c7ef33ca8930c8ef89d937027aa151c25c0
ManoharNaid <126132682+ManoharNaid@users.noreply.github.com>
2024-12-02 10:49:48 +0530

    Update HelloManu.java

--- HelloManu.java
+++ HelloManu.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Print a message to the console
-        System.out.println("Hello, Manu!");
+        System.out.println("Hello, Manu!")
         System.out.println("Hello, Manu! from fusion5");
     }
 }
